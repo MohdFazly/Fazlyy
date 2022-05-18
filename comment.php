5194d0d0c74ecd771e21fa6369ab186fd709c227
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-18 14:40:01 +0800

    sf

--- comment.php
+++ comment.php
@@ -10,7 +10,27 @@
     <?php
     $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
     'October', 'November', 'December');
+
     sort($month);
+    $i = 0;
+    for($i = 0; $i <= 11; $i++)
+    {
+        echo $month[$i]. "\n";
+    }
     ?>
+
+    <?php
+    echo "<br>";
+    echo "<br>";
+    foreach ($month as $key => $val) { 
+        echo "The value of key $key is: $val<br>"; 
+        } 
+        /* Only retrieves the value for the current element and 
+        ignores the
+        key */ 
+        foreach ($month as $val) { 
+        echo "Value: $val<BR>"; }
+    ?>
+
 </body>
 </html>
