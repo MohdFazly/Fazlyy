2f7c921979faabed289409af571008cd522e945c
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-19 23:07:36 +0800

    ghgh

--- comment.php
+++ comment.php
@@ -10,24 +10,16 @@
     <?php
     $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
     'October', 'November', 'December');
-
-    $array = explode (separator, $string);
-    $string ='Jan-Feb-Mac-Apr-May';
-    $array = explode ('-', $string);
     ?>
 
     <?php
-    echo "<br>";
-    echo "<br>";
-    foreach ($month as $key => $val) { 
-        echo "The value of key $key is: $val<br>"; 
-        } 
-        /* Only retrieves the value for the current element and 
-        ignores the
-        key */ 
-        foreach ($month as $val) { 
-        echo "Value: $val<BR>"; }
+    $array = explode (separator, $string);
+    $string ='Jan-Feb-Mac-Apr-May';
+    $array = explode ('-', $string);
+    
     ?>
+
+
 
 </body>
 </html>
