ecec0f8971a521ee914dcc889f789213da1a60ad
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-19 23:31:24 +0800

    aweafrest

--- comment.php
+++ comment.php
@@ -7,17 +7,24 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
-    'October', 'November', 'December');
-    ?>
 
-    <?php
-    $array = explode (separator, $string);
-    $string ='Jan-Feb-Mac-Apr-May';
-    $array = explode ('-', $string);
-    
-    ?>
+<?php
+//answer Question b
+$monthDays = array ('January' => 31, 'February' => 28, 'March' => 31,'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31,'September' => 30, 'October' => 31, 'November'=> 30, 'December'=> 31);
+foreach ($monthDays as $key => $day)
+{
+ echo "$key $day <br>";
+}
+echo "<br>";
+
+//answer of question c
+foreach($monthDays as $key => $day30)
+{
+    if($day30 == 30){
+        echo "$key $day30 <br>";
+    }
+}
+?>
 
 
 
